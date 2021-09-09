2cf1100b83ca1e25cbaf0b7e375725bf89a6f728
Lynda <hadjablynda99@gmail.com>
2021-09-09 14:34:36 +0200

    fix: fif conflict

--- backend/src/main/java/server/repositories/RoomRepository.java
+++ backend/src/main/java/server/repositories/RoomRepository.java
@@ -2,8 +2,11 @@ package server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import server.models.Department;
 import server.models.Room;
 import server.models.RoomId;
+
+import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, RoomId> {
