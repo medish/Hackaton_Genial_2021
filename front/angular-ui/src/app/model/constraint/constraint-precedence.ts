7658822ff0c8f2ef0339b7f7a7c9cd667c3b26f5
llp0702 <massyldjemai@gmail.com>
2021-09-09 11:14:07 +0200

    Parsing d'input

--- front/angular-ui/src/app/model/constraint/constraint-precedence.ts
+++ front/angular-ui/src/app/model/constraint/constraint-precedence.ts
@@ -1,7 +1,11 @@
 import { Selector } from "../selector/selector";
-import { Constraint } from "./constraint";
 
 export class ConstraintPrecedence{
+    selector:Selector={
+        selectorUnits:[]
+    };
+    veut: boolean = true;
+    priority:number=0; // This attribute should be between 0 and 100 
     precedence:string='before'; //Possible values for this attribute : 'before'|'after'|'synchro'
     strict:boolean=false;
     selectorTarget:Selector={selectorUnits:[]};
