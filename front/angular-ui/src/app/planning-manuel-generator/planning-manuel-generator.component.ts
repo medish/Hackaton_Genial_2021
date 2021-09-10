775e46a768f3c49c158b46349689517509357e2a
Louis Castelbou <louis13castelbou75@gmail.com>
2021-09-10 14:27:32 +0200

    bouton export ICS dans manuel

--- front/angular-ui/src/app/planning-manuel-generator/planning-manuel-generator.component.ts
+++ front/angular-ui/src/app/planning-manuel-generator/planning-manuel-generator.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import {FullCalendarComponent} from "@fullcalendar/angular";
 import uniqid from 'uniqid';
 import {createEvents} from 'ics';
+import {document} from "ngx-bootstrap/utils";
 
 @Component({
   selector: 'app-planning-manuel-generator',
@@ -170,28 +171,49 @@ export class PlanningManuelGeneratorComponent implements OnInit {
     };
   }
 
+  download(filename, input) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8, '+encodeURIComponent(input));
+    element.setAttribute('download', filename);
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+
   exportToICS() {
     let allEvents = this.getAllEvents();
     let events = [];
     for (let i = 0; i < allEvents.length; i++) {
       let event = allEvents[i];
+      console.log(event.extendedProps);
       let start = [
         event.start.getFullYear(),
         event.start.getMonth()+1,
         event.start.getDate(),
-        event.start.getHours(),
+        event.start.getUTCHours(),
         event.start.getMinutes()];
+      let hours = Math.floor(event.extendedProps["duration"]/60);
+      let minutes = event.extendedProps["duration"]%60;
+      console.log(event.extendedProps["duration"]);
+      if (isNaN(hours))
+        hours = 0;
+      if (isNaN(minutes))
+        minutes = 0;
       events.push({
         start : start,
-        duration : {hours : 3, minutes : 20},
-        title : event.title
+        duration : {hours :  hours, minutes : minutes},
+        title : event.title,
+        location : event.extendedProps["room"],
+        description : event.extendedProps["prof"]
       });
     }
     const {error, value} = createEvents(events);
     if (error)
       console.log(error);
-    else
-      console.log(value);
+    else {
+      var filename = "calendar.ics";
+      this.download(filename, value);
+    }
   }
 
   roomChangeHandler(roomNum: number) {
