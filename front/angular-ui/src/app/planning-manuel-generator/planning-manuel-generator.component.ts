30c9ed10e2bd8b362bc4690411be76fb97aecdce
ndione <ndionebaba@gmail.com>
2021-09-10 09:50:46 +0200

    Planning to json pour preparer la vérification

--- front/angular-ui/src/app/planning-manuel-generator/planning-manuel-generator.component.ts
+++ front/angular-ui/src/app/planning-manuel-generator/planning-manuel-generator.component.ts
@@ -3,9 +3,9 @@ import {Modal} from 'bootstrap';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin, {Draggable} from '@fullcalendar/interaction';
-import { DataInterfaceService } from '../services/data-interface.service';
-import { Class, Room, Degree, Teacher} from '../model/datastore/datamodel';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {DataInterfaceService} from '../services/data-interface.service';
+import {Class, Room, Degree, Teacher} from '../model/datastore/datamodel';
+import {FormBuilder, FormGroup} from '@angular/forms';
 import {FullCalendarComponent} from "@fullcalendar/angular";
 
 
@@ -26,39 +26,82 @@ export class PlanningManuelGeneratorComponent implements OnInit {
   teachers: Teacher[] = [];
   degrees: Degree[] = [];
   that = this;
-  id_event_clicked: string="";
-  calendarApi :any;
+  id_event_clicked: string = "";
+  calendarApi: any;
 
-  constructor(private dataService : DataInterfaceService, private fb : FormBuilder) {
+  constructor(private dataService: DataInterfaceService, private fb: FormBuilder) {
   }
 
   @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
-  modelData:{title:string} = {title:''};
+  modelData: { title: string } = {title: ''};
 
-  deleteEvent(id_event_clicked:string){
+  /**
+   * Delete event.
+   *
+   * @param id_event_clicked
+   */
+  deleteEvent(id_event_clicked: string) {
     let calendarApi = this.calendarComponent.getApi();
     let event = calendarApi.getEventById(id_event_clicked);
     event.remove()
   }
 
-  getAllEvents(){
+  /**
+   * Get All Events.
+   * @return Array
+   */
+  getAllEvents() {
     return this.calendarComponent.getApi().getEvents();
   }
-  prepareVerification(){
-    let arrayEvents= this.getAllEvents();
+
+  /**
+   * Get current Planning
+   */
+  prepareVerification() {
+
+    let arrayEvents = this.getAllEvents();
+    let arrayPrepared = [];
     console.warn(arrayEvents)
-    for(let i =0;i<arrayEvents.length;i++){
+    for (let i = 0; i < arrayEvents.length; i++) {
+      let dateStartStr = new Date(arrayEvents[i].startStr)
+
+      var userTimezoneOffset = dateStartStr.getTimezoneOffset() * 60000;
+      dateStartStr = new Date(dateStartStr.getTime() + userTimezoneOffset);
+      let startTime=dateStartStr.getHours()+":"+dateStartStr.getMinutes()+":"+dateStartStr.getSeconds();
+      let dateEndStr=new Date( new Date(arrayEvents[i].endStr).getTime() + userTimezoneOffset)
+      let endTime="";
+      if(!isNaN(dateEndStr.getTime())){
+         endTime = dateEndStr.getHours()+":"+dateEndStr.getMinutes()+":"+dateEndStr.getSeconds();
+      }else{
+        let defaulthour=dateStartStr.getHours()+1;
+        endTime = defaulthour+":"+dateStartStr.getMinutes()+":"+dateStartStr.getSeconds();
+      }
+      console.warn(dateEndStr.toLocaleDateString())
+      console.warn(endTime);
+      let dayNumber = dateStartStr.getDay()
+
+      arrayPrepared.push(
+        {
+          "id": arrayEvents[i].id,
+          "title": arrayEvents[i].title,
+          "startTime": startTime,
+          "endTime": endTime,
+          "dayNumber": dayNumber
+        }
+      )
       console.warn(arrayEvents[i]["id"])
-      alert(arrayEvents[i]["id"])
     }
+    console.warn(arrayPrepared)
+
+    return arrayPrepared;
   }
 
 
   ngOnInit() {
     let draggableEl = document.getElementById('external-events');
 
-        this.roomsForm = this.fb.group({
+    this.roomsForm = this.fb.group({
       roomControl: ['Choisir la salle ou l\'amphi']
     })
     this.classForm = this.fb.group({
@@ -83,13 +126,13 @@ export class PlanningManuelGeneratorComponent implements OnInit {
       itemSelector: '.fc-event',
       eventData: function (eventEl: any) {
         console.warn("From draggable Manuel")
-        let eventInitialColors={td:"#0d6efd",cours:"#dc3545",tp:"#ffc107"}
-        let target_color= eventEl.innerText.toLowerCase()
+        let eventInitialColors = {td: "#0d6efd", cours: "#dc3545", tp: "#ffc107"}
+        let target_color = eventEl.innerText.toLowerCase()
 
         return {
           title: eventEl.innerText,
-          id:Math.random(),
-          color:eventInitialColors[target_color]
+          id: Math.random(),
+          color: eventInitialColors[target_color]
         };
       }
     });
@@ -110,7 +153,8 @@ export class PlanningManuelGeneratorComponent implements OnInit {
       slotMinTime: "8:00:00",
       slotMaxTime: "20:00:00",
       firstDay: 1,
-      eventClick:  (info)=>{
+      //defaultTimedEventDuration:'02:00',
+      eventClick: (info) => {
         console.log(info.event.startStr)
         console.log(info.event.endStr);
         const day = new Date(info.event.startStr).getDay();
@@ -119,10 +163,10 @@ export class PlanningManuelGeneratorComponent implements OnInit {
         });
 
         // modify the id_event_clicked
-        this.id_event_clicked=info.event.id;
-        console.warn("Yeahhhh "+this.id_event_clicked)
-
-        this.modelData = {title:info.event.title}
+        this.id_event_clicked = info.event.id;
+        console.warn("Yeahhhh " + this.id_event_clicked)
+        this.prepareVerification()
+        this.modelData = {title: info.event.title}
         myModal.show();
       },
       eventAdd: function (addInfo) {
@@ -137,27 +181,27 @@ export class PlanningManuelGeneratorComponent implements OnInit {
 
   }
 
-  onClassesReceived(classes : [Class], context : this) {
-    for(let classItem of classes) {
+  onClassesReceived(classes: [Class], context: this) {
+    for (let classItem of classes) {
       context.classes.push(classItem);
     }
   }
 
-  onRoomsReceived(roomsReceived : [Room], context : this) {
-    for(let room of roomsReceived) {
+  onRoomsReceived(roomsReceived: [Room], context: this) {
+    for (let room of roomsReceived) {
       context.roomsList.push(room);
     }
 
   }
 
   onTeachersReceived(teachers: [Teacher], context: this) {
-    for(let teacher of teachers) {
+    for (let teacher of teachers) {
       context.teachers.push(teacher);
     }
   }
 
-  onDegreesReceived(degrees : [Degree], context: this) {
-    for(let degree of degrees) {
+  onDegreesReceived(degrees: [Degree], context: this) {
+    for (let degree of degrees) {
       context.degrees.push(degree);
     }
   }
