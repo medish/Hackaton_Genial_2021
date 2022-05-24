7c5989867a2e61a3bce7dd5a3f966c9ecb8562aa
Biko Pougala <bpougala@gmail.com>
2022-05-24 09:53:23 +0200

    Resolving conflicts with main branch

--- front/angular-ui/src/app/app-routing.module.ts
+++ front/angular-ui/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {AdminComponent} from "./admin/admin.component";
 import {ProfComponent} from "./prof/prof.component";
