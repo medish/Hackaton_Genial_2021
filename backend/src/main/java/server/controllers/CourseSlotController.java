23c9a293075ec03e764c66e244fc6708154d7b74
llp0702 <massyldjemai@gmail.com>
2022-05-25 14:39:56 +0200

    feat : fetch data for manual calendar from db using new api

--- backend/src/main/java/server/controllers/CourseSlotController.java
+++ backend/src/main/java/server/controllers/CourseSlotController.java
@@ -1,10 +1,7 @@
 package server.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import server.models.CourseSlot;
 import server.models.CourseSlotId;
 import server.services.CourseSlotService;
