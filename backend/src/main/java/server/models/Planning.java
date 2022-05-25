9cf9275ad7ac0a388ee7a93dec93076abe7b3fd3
Damien HERBERT <dherbert98@gmail.com>
2022-05-25 11:48:11 +0200

    generate auto increment

--- backend/src/main/java/server/models/Planning.java
+++ backend/src/main/java/server/models/Planning.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 public class Planning {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
--- backend/src/main/java/server/models/PrecedenceConstraint.java
+++ backend/src/main/java/server/models/PrecedenceConstraint.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class PrecedenceConstraint implements IInput {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String selector;
