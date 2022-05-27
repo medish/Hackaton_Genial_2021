c04c207313968e17a350447053be0b5e5ee0875f
Biko Pougala <bpougala@gmail.com>
2022-05-27 09:09:11 +0200

    WIP

--- front/angular-ui/src/app/tableau-contraintes/tableau-contraintes.component.ts
+++ front/angular-ui/src/app/tableau-contraintes/tableau-contraintes.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ConstraintPrecedence } from '../model/constraint/constraint-precedence';
-import { ConstraintTimeRoom } from '../model/constraint/constraint-time-room';
+import { ConstraintPrecedence, ConstraintPrecedenceExport } from '../model/constraint/constraint-precedence';
+import { ConstraintTimeRoom, ConstraintTimeRoomExport } from '../model/constraint/constraint-time-room';
 import { DataInterfaceService } from '../services/data-interface.service';
+import {HttpClient} from "@angular/common/http";
+import {DateSlotDayEnum, TimeconstraintcontrollerApi} from "../model/swagger/api";
 
 
 let CONSTRAINTS_TIME_AND_ROOM: ConstraintTimeRoom[] = [
@@ -14,7 +16,7 @@ let CONSTRAINTS_TIME_AND_ROOM: ConstraintTimeRoom[] = [
           value:'c',}
         ]
     },
-    veut: true,
+    wants: true,
     priority:0, // This attribute should be between 0 and 100
     room:{selectorUnits:[
      { table: "",
@@ -22,8 +24,8 @@ let CONSTRAINTS_TIME_AND_ROOM: ConstraintTimeRoom[] = [
       value: "19",}
     ]},
     day:1,
-    hourBegin:'08:00',
-    hourEnd:'12:00',
+    dateBegin:'08:00',
+    dateEnd:'12:00',
 }
 ];
 const CONSTRAINTS_PRECEDENCE:ConstraintPrecedence[]=[
@@ -32,9 +34,9 @@ const CONSTRAINTS_PRECEDENCE:ConstraintPrecedence[]=[
     selector:{
       selectorUnits:[{table:'ens',attribute:'name',value:'zielonka'}]
     },
-    veut:true,
+    wants:true,
     priority:50,
-    precedence:'before',
+    whenConstraint:'before',
     strict:true,
     selectorTarget:{
       selectorUnits:[{table:'ens',attribute:'name',value:'klimann'}]
@@ -61,12 +63,64 @@ export class TableauContraintesComponent implements OnInit, OnChanges {
 
   @Input("constraintsTimeRoom") constraintsTimeRoom = CONSTRAINTS_TIME_AND_ROOM;
   @Input("constraintPrecedence") constraintPrecedence = CONSTRAINTS_PRECEDENCE;
-  constructor(private serv:DataInterfaceService) { }
+  constructor(private serv:DataInterfaceService, private http : HttpClient) { }
   ngOnInit(): void {
     this.serv.fetchPrecedenceConstraints((data)=>{this.constraintPrecedence = data})
     this.serv.fetchTimeConstraints((data)=>{this.constraintsTimeRoom=data})
   }
 
   ngOnChanges(changes: SimpleChanges) {
+    let dataService = new DataInterfaceService(this.http);
+    for (let propName in changes) {
+      let change = changes[propName];
+      if(change.currentValue.length != 0) {
+        console.log("ici");
+        if (change.currentValue[0].whenConstraint != null) {
+          //let precedenceConstraint: [ConstraintPrecedence] = change.currentValue;
+          let precedenceConstraint: [ConstraintPrecedenceExport] = [null];
+          precedenceConstraint.pop();
+          let prec : [ConstraintPrecedence] = change.currentValue;
+          for (let i = 0; i < prec.length; i++) {
+            const curr : ConstraintPrecedence = prec[i];
+            let whenConstraint = curr.whenConstraint == "before" ? "avant" : curr.whenConstraint == "pendant" ? "synchro" : "apres";
+            let tmp: ConstraintPrecedenceExport = {
+              id: curr.id,
+              selector: curr.selector.selectorUnits[0].table+":"+curr.selector.selectorUnits[0].attribute+":"+curr.selector.selectorUnits[0].value,
+              wants: curr.wants,
+              whenConstraint: whenConstraint,
+              strict: curr.strict,
+              target: curr.selectorTarget.selectorUnits[0].table+":"+curr.selectorTarget.selectorUnits[0].attribute+":"+curr.selectorTarget.selectorUnits[0].value,
+              priority: curr.priority
+            }
+            precedenceConstraint.push(tmp);
+          }
+          console.log(precedenceConstraint);
+          dataService.sendPrecedenceConstraints(precedenceConstraint);
+        } else {
+          console.log("salut");
+          let timeRoomConstraint : [ConstraintTimeRoom] = change.currentValue;
+          let timeRoomConstraintExp: [ConstraintTimeRoomExport] = [null];
+          timeRoomConstraintExp.pop();
+          let t = new TimeconstraintcontrollerApi();
+          for(let i = 0; i < timeRoomConstraint.length; i++) {
+            const curr : ConstraintTimeRoom = timeRoomConstraint[i];
+            let day : DateSlotDayEnum = curr.day == 1 ? "MONDAY" : curr.day == 2 ? "TUESDAY" : curr.day == 3 ? "WEDNESDAY" : curr.day == 4 ? "TUESDAY" : "FRIDAY";
+            let start = curr.dateBegin.split(":");
+            let end = curr.dateEnd.split(":");
+            let tmp : ConstraintTimeRoomExport = {
+              id: curr.id,
+              selector: curr.selector.selectorUnits[0].table+":"+curr.selector.selectorUnits[0].attribute+":"+curr.selector.selectorUnits[0].value,
+              wants: curr.wants,
+              dateBegin: {day: day, startTime: {hour: parseInt(start[0]), minute: parseInt(start[1]), second: parseInt(start[2])}},
+              dateEnd: {day: day, startTime: {hour: parseInt(end[0]), minute: parseInt(end[1]), second: parseInt(end[2])}},
+              room: curr.room.selectorUnits[0].table+":"+curr.room.selectorUnits[0].attribute+":"+curr.room.selectorUnits[0].value,
+              priority: curr.priority
+            }
+            timeRoomConstraintExp.push(tmp);
+          }
+          dataService.sendTimeRoomConstraints(timeRoomConstraintExp);
+        }
+      }
+    }
   }
 }
