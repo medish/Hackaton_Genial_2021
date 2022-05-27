2a5a199ae586c211791ba3334a401a14dffc9739
Damien HERBERT <dherbert98@gmail.com>
2022-05-27 11:00:42 +0200

    add insert and update in controllers

--- backend/src/main/java/server/controllers/CourseGroupController.java
+++ backend/src/main/java/server/controllers/CourseGroupController.java
@@ -1,16 +1,19 @@
 package server.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import server.models.CourseGroup;
 import server.models.CourseGroupId;
 import server.services.CourseGroupService;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(ControllerRoutes.COURSES_GROUPS)
@@ -20,12 +23,18 @@ public class CourseGroupController {
     private CourseGroupService service;
 
     @GetMapping()
-    public List<CourseGroup> getAll(){
+    public List<CourseGroup> getAll() {
         return service.getAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<CourseGroup> getById(@PathVariable CourseGroupId id){
+    public Optional<CourseGroup> getById(@PathVariable CourseGroupId id) {
         return service.getById(id);
+    }
+
+    @PostMapping()
+    public boolean insert(@RequestBody CourseGroup courseGroup) {
+        service.insert(courseGroup);
+        return true;
     }
 }
