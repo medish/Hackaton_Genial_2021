1abb95d81454b76572bb155b1915e5a0f1b142d5
Quentin <quentin.lenoir@etu.u-paris.fr>
2022-05-27 11:37:03 +0200

    Debut refacto Time Constraint Form

--- front/angular-ui/src/app/constraints/form-constraint-time/form-constraint-time.component.ts
+++ front/angular-ui/src/app/constraints/form-constraint-time/form-constraint-time.component.ts
@@ -4,6 +4,8 @@ import { ConstraintService } from '../../services/constraint/constraint.service'
 import {addUnitPriority} from "ngx-bootstrap/chronos/units/priorities";
 import {Selector} from "../../model/selector/selector";
 import {SelectorUnit} from "../../model/selector/selector-unit";
+import {Professor,ProfessorcontrollerApi,Course,CoursecontrollerApi,Room,RoomcontrollerApi} from '../../model/swagger/api'
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -13,12 +15,20 @@ import {SelectorUnit} from "../../model/selector/selector-unit";
 })
 export class FormConstraintTimeComponent implements OnInit {
 
-  constructor(public constraintService:ConstraintService) { }
+  constructor(
+    public constraintService:ConstraintService,
+    private professorController: ProfessorcontrollerApi,
+    private courseController: CoursecontrollerApi,
+    private roomController: RoomcontrollerApi,
+    private formBuilder: FormBuilder
+  ) {}
+
+
   @Output('onValidate')onValidate = new EventEmitter<ConstraintTimeRoom>()
 
 
   days = [
-    'lundi','mardi', 'mercredi', 'jeudi', 'vendredi'
+    'Lundi','Mardi', 'Mercredi', 'Jeudi', 'Vendredi'
   ];
 
   wants = [
@@ -26,8 +36,25 @@ export class FormConstraintTimeComponent implements OnInit {
     'ne souhaite pas'
   ];
 
+  selectors = [
+    {
+      label:'Professeur',
+      value:'teacher'
+    },
+    {
+      label: 'Cours',
+      value: 'lesson'
+    }
+  ]
+
+  professors : Professor[] = [];
+  courses : Course[] = [];
+  rooms : Room[] = [];
+  timeFormGroup: FormGroup;
+
   timeConstraintForm = {
     selector: '',
+    selectorTarget: '',
     want: '',
     day: null,
     startTime: '',
@@ -38,8 +65,12 @@ export class FormConstraintTimeComponent implements OnInit {
 
   onSubmit() {
 
+    if(this.timeFormGroup.invalid){
+      return;
+    }
+
     let form_result= [
-      this.timeConstraintForm.selector,
+      this.timeConstraintForm.selector+":id:"+this.timeConstraintForm.selectorTarget,
       this.timeConstraintForm.want == 'souhaite' ? 'true':'false',
       (this.days.indexOf(this.timeConstraintForm.day)+1).toString(),
       this.timeConstraintForm.startTime+":00",
@@ -73,6 +104,35 @@ export class FormConstraintTimeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.professorController.getAllUsingGET8().then(data=>{
+      for(let prof of data){
+        this.professors.push(prof);
+      }
+    });
+
+    this.courseController.getAllUsingGET().then(data=>{
+      for(let course of data){
+        this.courses.push(course);
+      }
+    });
+
+    this.roomController.getAllUsingGET9().then(data=>{
+      for(let room of data){
+        this.rooms.push(room);
+      }
+    });
+
+    this.timeFormGroup = this.formBuilder.group({
+      selector: ['', [Validators.required]],
+      teacher: ['', [Validators.required]],
+      lesson: ['', [Validators.required]],
+      want: ['', [Validators.required]],
+      day: ['', [Validators.required]],
+      startTime: ['', [Validators.required]],
+      endTime: ['', [Validators.required]],
+      room: ['', [Validators.required]],
+      priority: ['',[Validators.required]]
+    });
   }
 
 }
